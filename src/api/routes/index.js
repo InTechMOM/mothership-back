7fc7a071497aaf7ad0262a897d5b73f26312a473
Mariana Barrios <barriosmarianavictoria16@gmail.com>
2023-12-04 20:45:14 -0300

    add files

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -3,7 +3,32 @@ import swaggerJsdoc from "swagger-jsdoc";
 import { serve, setup } from "swagger-ui-express";
 import swaggerSpecification from "../../config/swagger.js";
 
+
+
+import {
+  validateCreation,
+  validatePatch,
+}  from './validation/post.js'
+
+import {  
+  getByName,
+} from './controllers/get.js';
+
+import {
+  createUser, 
+} 
+from './controllers/post.js'
+import editUser from './controllers/patch.js';
+import deleteOne from './controllers/delete.js'
+
+
 const router = Router();
+
+router
+  .post('/user', validateCreation, createUser)
+  .get('/user',getByName)
+  .patch('/user/:id', validatePatch, editUser)
+  .delete('/user/:id', validateCreation, deleteOne)
 
 router.use("/docs", serve);
 router.get("/docs", setup(swaggerJsdoc(swaggerSpecification)));
