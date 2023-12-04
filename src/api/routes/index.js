36ef64207f9a9f1eea36d4cd27d50f3dfec93283
Karen <karen-0212@hotmail.com>
2023-12-04 11:20:04 -0300

    add routers

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,10 +2,20 @@ import { Router } from "express";
 import swaggerJsdoc from "swagger-jsdoc";
 import { serve, setup } from "swagger-ui-express";
 import swaggerSpecification from "../../config/swagger.js";
+import audioRouter from "./graphic-content/audioRouter.js";
+import descriptionRouter from "./graphic-content/descriptionRouter.js";
+import generateRouter from "./graphic-content/generateRouter.js";
+import imageRouter from "./graphic-content/imageRouter.js";
 
 const router = Router();
 
 router.use("/docs", serve);
 router.get("/docs", setup(swaggerJsdoc(swaggerSpecification)));
+
+// graphic=content
+router.use("/", audioRouter);
+router.use("/", descriptionRouter);
+router.use("/", generateRouter);
+router.use("/", imageRouter);
 
 export default router;
